8db7d985741b08a1adb8dac386f0f0bc28d0a4ef
Sergei Dudka <dudk@uber.com>
2021-01-16 22:16:59 +0100

    add single url crawler

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"net/url"
 	"os"
+	"sync"
 )
+
+type crawler struct {
+	wg      sync.WaitGroup
+	m       sync.Mutex
+	visited map[string]struct{}
+}
 
 type flags struct {
 	rawURL string
@@ -22,6 +31,41 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+
+	c := crawler{
+		visited: make(map[string]struct{}),
+	}
+
+	<-c.Start(context.Background(), f.rawURL)
+}
+
+func (c *crawler) Start(ctx context.Context, url string) chan struct{} {
+	done := make(chan struct{})
+	defer close(done)
+	c.wg.Add(1)
+	go c.fetch(ctx, url, url)
+	c.wg.Wait()
+	return done
+}
+
+func (c *crawler) fetch(ctx context.Context, url, baseURL string) {
+	defer c.wg.Done()
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		unhandled(err)
+	}
+
+	res, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		unhandled(err)
+	}
+
+	_ = parse(res)
+}
+
+func parse(res *http.Response) []string {
+	fmt.Println(res.Header.Get("Content-Type"))
+	return nil
 }
 
 func (f flags) validate() error {
@@ -52,4 +96,8 @@ func (f flags) validate() error {
 	}
 
 	return nil
+}
+
+func unhandled(err error) {
+	panic(fmt.Errorf("unhandled error: %w", err))
 }
