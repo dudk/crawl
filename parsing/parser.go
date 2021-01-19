107e6b34c9e35510fb34edb24da8db7cd39dc657
Sergei Dudka <dudk@uber.com>
2021-01-19 23:54:31 +0100

    use visitor in parser

--- parsing/parser.go
+++ parsing/parser.go
@@ -9,47 +9,60 @@ import (
 	"golang.org/x/net/html"
 )
 
+// Visitor records if parsed URL was visited. It's safe for concurrent
+// use.
+type Visitor interface {
+	Visit(string) bool
+}
+
 // HTML is dead-stupid html parser. It uses standard library url package to
 // normalize URLs. Only URLs having base URL as prefix will be returned by the parser.
 type HTML struct {
 	BaseURL string
+	Visitor
 }
 
 // ParseBody parses HTML for URLS. Only <a> tags are examined and exact URL
 // matching is made.
-func (p HTML) ParseBody(r io.Reader) ([]string, error) {
+func (p HTML) ParseBody(url *url.URL, r io.Reader) ([]string, error) {
 	n, err := html.Parse(r)
 	if err != nil {
 		return nil, fmt.Errorf("HTML parsing error: %w", err)
 	}
 
 	var links []string
-	return parseLinks(links, n, p.BaseURL), nil
+	return parseLinks(links, p.Visitor, n, url, p.BaseURL), nil
 }
 
 // recursive depth-first html parsing.
-func parseLinks(links []string, n *html.Node, baseURL string) []string {
+func parseLinks(links []string, v Visitor, n *html.Node, url *url.URL, baseURL string) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key != "href" {
 				continue
 			}
-			if URL, ok := normalize(a.Val); ok && strings.HasPrefix(URL, baseURL) {
-				links = append(links, a.Val)
+			if URL, ok := normalize(url, a.Val); ok && strings.HasPrefix(URL, baseURL) && !v.Visit(URL) {
+				links = append(links, URL)
 			}
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = parseLinks(links, c, baseURL)
+		links = parseLinks(links, v, c, url, baseURL)
 	}
 	return links
 }
 
 // validate and normalize raw URL. returns false if URL cannot be parsed.
-func normalize(URL string) (string, bool) {
+func normalize(baseURL *url.URL, URL string) (string, bool) {
 	u, err := url.Parse(URL)
 	if err != nil {
 		return URL, false
 	}
+	u.Fragment = ""
+	if u.IsAbs() {
+		return u.String(), true
+	}
+
+	u = baseURL.ResolveReference(u)
 	return u.String(), true
 }
--- parsing/parsing_test.go
+++ parsing/parsing_test.go
@@ -2,20 +2,25 @@ package parsing_test
 
 import (
 	"bytes"
+	"net/url"
+	"reflect"
 	"testing"
 
+	"github.com/dudk/crawl/caching"
 	"github.com/dudk/crawl/parsing"
 )
 
 func TestParser(t *testing.T) {
 	p := parsing.HTML{
 		BaseURL: "http://base.org",
+		Visitor: caching.NewInMemoryCache(),
 	}
-	results := []string{
+	expected := []string{
 		"http://base.org/a",
 		"http://base.org/b",
 		"http://base.org/c",
 		"http://base.org/c/d",
+		"http://base.org/c/f",
 	}
 
 	buf := bytes.NewBuffer([]byte(`<html>
@@ -26,13 +31,17 @@ func TestParser(t *testing.T) {
 		<a href="http://base.org/c"></a>
 		<a href="http://base.org/c/d"></a>
 		<a href="http://base.net/e"></a>
-		<a href="https://base.org/f"></a>
+		<a href="f"></a>
 	</body>
 	</html>`))
+	u, _ := url.Parse("http://base.org/c/")
+	links, _ := p.ParseBody(u, buf)
+	assertEqual(t, "urls", links, expected)
+}
 
-	links, _ := p.ParseBody(buf)
-	if len(links) != len(results) {
-		t.Fatalf("expected: %v got: %v", results, links)
+func assertEqual(t *testing.T, name string, result, expected interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("%v\nresult: \t%T\t%+v \nexpected: \t%T\t%+v", name, result, result, expected, expected)
 	}
-
 }
