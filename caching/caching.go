e847c74069a6500b3b1e237f37f85a12405a910e
Sergei Dudka <dudk@uber.com>
2021-01-20 11:18:02 +0100

    add mutex to caching

--- caching/caching.go
+++ caching/caching.go
@@ -1,19 +1,25 @@
 package caching
 
+import "sync"
+
 // InMemoryCache uses built-in map type as a storage for cache.
 type InMemoryCache struct {
+	m     *sync.Mutex
 	cache map[string]struct{}
 }
 
 // NewInMemoryCache returns initialized in-memory cache.
 func NewInMemoryCache() InMemoryCache {
 	return InMemoryCache{
+		m:     &sync.Mutex{},
 		cache: make(map[string]struct{}),
 	}
 }
 
 // Visit returns true if provided string was visited before.
 func (c InMemoryCache) Visit(s string) bool {
+	c.m.Lock()
+	defer c.m.Unlock()
 	if _, ok := c.cache[s]; ok {
 		return true
 	}
